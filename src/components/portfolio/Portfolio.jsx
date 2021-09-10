3ab134105de9791fe741ab19c502342b4676152e
TrevorMadeSomething <54298052+TrevorMadeSomething@users.noreply.github.com>
2021-09-10 15:32:40 -0400

    added portfolio dummy data

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,52 @@
+import { useState } from "react"
+import PortfolioList from "../portfolioList/PortfolioList"
 import "./portfolio.scss"
-
+import { featuredPortfolio, webPortfolio, mobilePortfolio, designPortfolio, contentPortfolio } from "../../data";
 export default function Portfolio() {
+
+    const [selected, setSelected] = useState("featured");
+
+    const list = [
+        {
+            id: "featured",
+            title: "Featured"
+        },
+        {
+            id: "web",
+            title: "Web App"
+        },
+        {
+            id: "mobile",
+            title: "Mobile App"
+        },
+        {
+            id: "design",
+            title: "Design"
+        },
+        {
+            id: "branding",
+            title: "Branding"
+        },
+        {
+            id: "content",
+            title: "Content"
+        }
+    ]
+
     return (
         <div className="portfolio" id="portfolio">
-
+            <h1>Portfolio</h1>
+            <ul>
+                {list.map(item => (
+                    <PortfolioList title={item.title} active={selected === item.id} setSelected={setSelected} id={item.id} />
+                ))}
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/timeTracker.png" alt="" />
+                    <h3>Time Tracker App</h3>
+                </div>
+            </div>
         </div>
     )
 }
