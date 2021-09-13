504df6bcb2653b49f2bcdc25ecb90a287322cc89
TrevorMadeSomething <54298052+TrevorMadeSomething@users.noreply.github.com>
2021-09-13 16:45:13 -0400

    added email support

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,27 +1,45 @@
-import { useState } from "react";
 import "./contact.scss"
+import { useState } from "react";
+import emailjs from 'emailjs-com';
 
 export default function Contact() {
 
     const [message, setMessage] = useState(false);
 
-
     const handleSubmit = (e) => {
         e.preventDefault();
+
         setMessage(true);
     }
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('service_2fy8cgm', 'template_4rr6dkn', e.target, 'user_7bGx35LhZsIhc1ISPClIA')
+            .then((result) => {
+                console.log(result.text);
+                console.log(893472984732987423);
+            }, (error) => {
+                console.log(error.text);
+            });
+
+        handleSubmit(e);
+    }
+
+
+
     return (
         <div className="contact" id="contact">
-            <form>
+            <form onSubmit={sendEmail}>
                 <div className="left">
                     <h2>I would <span className="love">love</span> to hear from you!</h2>
                     <div className="fieldContainer">
-                        <input type="text" placeholder="Name" className="name" />
-                        <input type="text" placeholder="Email" className="email" />
+                        <input type="text" placeholder="Name" className="name" name="name" />
+                        <input type="text" placeholder="Email" className="email" name="email" />
                     </div>
                     <div className="right">
                         <textarea placeholder="Message"></textarea>
-                        <button onClick={handleSubmit}>Submit</button>
+                        <input type="submit" className="btn" value="Send" />
                         {message && <span>Thank you, I will reply ASAP</span>}
                     </div>
                 </div>
